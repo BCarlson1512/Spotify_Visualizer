ba0ee34762ce6cc68c0d68fd0c326c4503f637c5
BCarlson1512 <benproskilled@gmail.com>
2022-02-25 02:50:20 -0500

    Live song visualization

--- README.md
+++ README.md
@@ -10,13 +10,21 @@ Live Demo: Coming soon...
 
 ## Issues/Mental Blocks
 
+Data processing
+
 - Spotify API provides TONS of data, specifically related to audio analysis. Meaning that the MDN web audio API is not of any use here, since we don't have an audio file to process. Using Web Audio API, you can just plug a sound file into FFT (Fast Fourier Transform) to perform signal processing, whereas the data here needs to be formatted in a way that it can be roughly grouped into columns, that are ultimately shown. This was decided using a data structure of the current nature ```js {starttime, finishtime, frequency, loudness}```
+  
+2d Visualization issues...
+
+- Dynamically updating the waveform, in real time has been quite a journey. Initially the plan was to use multiple setstate functions inside of a useeffect before realizing that they all trigger re-renders (thanks react, love you) before the useeffect finishes, ultimately making them all undefined/null. The workaround here is to store all the required data into an object and then pass its data down to its respective components.
+- Never did I think that floating point errors would have such a deep rooted impact in a project, however that small discrepancy in rounding (usually 0.00001) created a HUGE issue in fetching the appropriate data from a set of over 50000 data points (for a track length of 3:11)
 
 ## Stages
 
 - [x] Connect to Spotify API
 - [x] Playback in web browser
 - [ ] Create 2D Audio Visualization
+- [ ] Sync visualization in real time
 - [ ] Implement Three.js Visualization
 - [ ] Clean up frontend code
 
@@ -34,5 +42,6 @@ cd into client and type npm run dev
 
 ## Future
 
+Attempting not to put my head through a wall on corner cases
 3D Modelling to take the epilepsy to the next level, (Three.js/webGL)
 Increased UX
--- client/src/components/TrackVisualizer.js
+++ client/src/components/TrackVisualizer.js
@@ -1,5 +1,5 @@
 import { Container, Box, Typography, Button } from '@mui/material';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 /**
@@ -19,7 +19,7 @@ const processSegments = (sections, segments) => {
             const vol = Math.abs(segment.loudness_max)
             for (const pitch of segment.pitches) {
                 const freq = startingFrequency * pitch;
-                segmentData.push({startTime: segment.start, finishTime: segment.start + segment.duration, frequency: freq, volume: vol})
+                segmentData.push({startTime: Math.ceil((segment.start) * 100) / 100, finishTime: Math.ceil((segment.start + segment.duration) * 100) / 100, frequency: freq, volume: vol * pitch});
             }
         }
     }
@@ -29,14 +29,55 @@ const processSegments = (sections, segments) => {
 /**
  * Updates the bar color/height from live data... TODO: Implement live data, rather than default results
  */
-const updateBarParams = (numBars) => {
+const updateBarParams = (numBars, bars) => {
     const barsData = []
     let i = 0;
     while(i < numBars) {
-        barsData[i] ={height: "7.5vh"}
+        barsData[i] ={height: `${1 + bars[i] % 15}vh`}
         ++i;
     }
     return barsData;
+}
+
+/**
+ * Compare callback for objects by frequency
+ * @param {*} a object a
+ * @param {*} b object b
+ * @returns -1 when freq(a) > freq(b) , 1 freq(a) < freq(b), 0 freq(a) === freq(b)
+ */
+const compare = (a,b) => {
+    if(a.frequency > b.frequency) {
+        return 1;
+    }
+    if(a.frequency < b.frequency) {
+        return -1;
+    }
+    return 0;
+}
+/**
+ * Generates waveform data at a specic time frame
+ * @param {*} freqBands -> A set of frequencies "buckets" to group data into
+ * @param {*} segments -> A set of segments belonging to the same start time
+ */
+const updateBars = (freqBands, segments) =>{
+    segments = segments.sort((a,b) => compare(a,b));
+    //console.log(segments);
+    //console.log(freqBands);
+    const ret = []; // store height and frequency
+    let lofreq = 0;
+    let segCount = 0;
+    for(let i = 0; i < freqBands.length; i++) { // initialize values
+        ret[i] = 0;
+    }
+    while (segCount < segments.length) { // loop over segments
+        while(segCount < segments.length && segments[segCount].frequency <= freqBands[lofreq]) { // partition accordingly
+            ret[lofreq] += segments[segCount].volume;
+            segCount++;
+        }
+        lofreq++;
+    }
+    
+    return ret;
 }
 
 const initFreqBands = (maxFreq, numBands) => {
@@ -76,6 +117,16 @@ const maxDuration = (segments) => {
     return max;
 }
 
+const maxFreq = (segments) => {
+    let max = -1;
+    for(const segment of segments) {
+        if(segment.frequency > max){
+            max = segment.frequency;
+        }
+    }
+    return max;
+}
+
 /**
  * Gets the min duration of segments
  */
@@ -99,22 +150,46 @@ export default function TrackVisualizer(props) {
     const {tatums} = props;
     const {track} = props;
     
-    // waveform variables
+    // waveform Constants
     const numBars = 50;
-    const freqBands = initFreqBands(track.analysis_sample_rate,numBars);
     const segData = processSegments(sections, segments);
-
-
-    const barConfig = updateBarParams(numBars);
+    const freqBands = initFreqBands(maxFreq(segData),numBars);
 
     // usestates
     const [displayAnalytics, setDisplayAnalytics] = useState(false);
     
+    /**
+     * Waveform state
+     */
+    const timeSigData = segData.filter(segment => segment.startTime === 0)
+    const barsStep = updateBars(freqBands, timeSigData);
+    const barConfig = updateBarParams(numBars, barsStep);
+    const [waveformState, setWaveFormState] = useState({
+        timeSignature: 0,
+        timeSigData: timeSigData,
+        barsConfig: barConfig,
+    });
     const analyticsClickHandler = (e) => {
         e.preventDefault();
         setDisplayAnalytics(!displayAnalytics);
     }
-    
+    console.log(segData)
+    /* hook for updating waveform data */
+    useEffect(() => {
+        if(waveformState.timeSignature === segData[segData.length - 1].finishTime) return;
+        console.log(waveformState)
+        let newData = segData.filter(segment => segment.startTime === waveformState.timeSignature)
+        console.log(newData);
+        const interval = setInterval(() =>{
+            let barSteps = updateBars(freqBands, waveformState.timeSigData);
+            setWaveFormState({
+                timeSignature: newData[0].finishTime,
+                timeSigData: newData,
+                barsConfig: updateBarParams(numBars, barSteps),
+            });
+        }, (newData[0].finishTime - newData[0].startTime))
+        return () => clearInterval(interval);
+    },[waveformState, segData, freqBands]);
     return (
         <Container>
         <Box sx={{display:"flex", justifyContent:"space-between"}}>
@@ -131,7 +206,7 @@ export default function TrackVisualizer(props) {
                 </Box>
             }
             <Box className="visualizer-container" sx={{display:"flex", paddingTop: "1vh"}}>
-                {barConfig.map(bar => (
+                {waveformState.barsConfig.map(bar => (
                     <Box sx={{backgroundColor:"primary.dark", height:bar.height, width:"1vw", marginLeft:"0.125vw"}}></Box>
                 ))}
             </Box>
